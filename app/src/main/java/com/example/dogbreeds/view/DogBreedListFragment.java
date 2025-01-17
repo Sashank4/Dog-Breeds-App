806d4be3b89358ad206ba2e93e5dab1439eb65c4
Sashank <sashank@FVFGP840Q05N.local>
2025-01-17 11:38:35 +0530

    checking pushing behavior

--- app/src/main/java/com/example/dogbreeds/view/DogBreedListFragment.java
+++ app/src/main/java/com/example/dogbreeds/view/DogBreedListFragment.java
@@ -12,6 +12,7 @@ import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+
 import com.example.dogbreeds.adapter.DogBreedAdapter;
 import com.example.dogbreeds.presenter.DogViewListPresenter;
 import com.example.dogbreeds.R;
